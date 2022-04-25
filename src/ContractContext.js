96cb72a3adc807e4da751ff680c88d86a6d6aacb
Kevin Dave <kevindavee1505@gmail.com>
2022-04-25 23:12:41 +0700

    contract context code and address

--- src/ContractContext.js
+++ src/ContractContext.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
+import { web3Config } from './config';
+import Abi from './abi.json';
 
 const ContractContext = React.createContext({});
 
-export const ContractContextProvider = ({ children }) => {
-    const contextValue = {
+export const ContractContextProvider = ({ children, provider, rpcProvider }) => {
+  const [contract, setContract] = useState(null);
+  const [readContract, setReadContract] = useState(null);
 
-    };
-  
-    return (
-      <ContractContext.Provider value={contextValue}>
-        {children}
-      </ContractContext.Provider>
-    );
+  useEffect(() => {
+    if (rpcProvider) {
+      const { contractAddress } = web3Config;
+      setReadContract(new ethers.Contract(
+        contractAddress, Abi, rpcProvider
+      ));
+    }
+  }, [rpcProvider]);
+
+  useEffect(() => {
+    if (provider) {
+      const { contractAddress } = web3Config;
+      setContract(new ethers.Contract(
+        contractAddress, Abi, provider
+      ));
+    }
+  }, [provider]);
+
+  const contextValue = {
+    contract: (purpose) => purpose === 'signing' ? contract : readContract,
+  };
+
+  return (
+    <ContractContext.Provider value={contextValue}>
+      {children}
+    </ContractContext.Provider>
+  );
 };
 
 export const useContract = () => {
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const web3Config = {
     chainId: Number(process.env.REACT_APP_CHAIN_ID),
     chainName: process.env.REACT_APP_CHAIN_NAME,
-    rpcUrl: process.env.REACT_APP_RPC_URL
+    rpcUrl: process.env.REACT_APP_RPC_URL,
+    contractAddress: process.env.REACT_APP_CONTRACT_ADDRESS
 }
