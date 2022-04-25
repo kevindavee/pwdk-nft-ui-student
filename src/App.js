48fae14d93fe16fccb660cbd9c2f9e9c129aa42f
Kevin Dave <kevindavee1505@gmail.com>
2022-04-25 21:15:24 +0700

    use WalletContext

--- src/App.js
+++ src/App.js
@@ -1,90 +1,16 @@
-import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ethers } from 'ethers';
-import detectEthereumProvider from '@metamask/detect-provider';
 import { web3Config } from './config';
+import { useWallet } from './WalletContext';
 
 function App() {
-  const [provider, setProvider] = useState();
-  const [rpcProvider, setRpcProvider] = useState();
-  const [address, setWalletAddress] = useState('');
-
-  useEffect(() => {
-    const checkConnectedWallet = async () => {
-      const signer = provider.getSigner();
-      try {
-        const address = await signer.getAddress();
-        setWalletAddress(address);
-      } catch (e) {
-        console.error(e.message);
-      }
-    }
-
-    const listenToWalletAddressChange = async () => {
-      const ethProvider = await detectEthereumProvider();
-      ethProvider.on('accountsChanged', (accounts) => {
-        setWalletAddress(accounts[0]);
-      });
-    }
-
-    const listenToNetworkChange = async () => {
-      const ethProvider = await detectEthereumProvider();
-      ethProvider.on('chainChanged', (networkId) => {
-
-      });
-    }
-
-    if (provider) {
-      checkConnectedWallet();
-      listenToWalletAddressChange();
-      listenToNetworkChange();
-    }
-  }, [provider]);
-
-  useEffect(() => {
-    const initProvider = async () => {
-      const ethProvider = await detectEthereumProvider();
-      if (!ethProvider) {
-        console.error('Metamask is not installed');
-        return;
-      }
-      const provider = new ethers.providers.Web3Provider(ethProvider);
-      setProvider(provider);
-    };
-
-    const initRpcProvider = () => {
-      const provider = new ethers.providers.JsonRpcProvider(web3Config.rpcUrl);
-      setRpcProvider(provider);
-    }
-
-    initProvider();
-    initRpcProvider();
-  }, []);
-
-  const connectToWallet = async () => {
-    try {
-      await provider.send('eth_requestAccounts', []);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      setWalletAddress(address);
-    } catch (e) {
-      console.error(e.message);
-    }
-  };
-
-  const switchToNetwork = async () => {
-    const { chainId } = web3Config;
-  
-    try {
-      await provider.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: `0x${chainId}`}],
-      })
-    } catch (e) {
-      console.error(e.message);
-    }
-  };
+  const {
+    provider,
+    rpcProvider,
+    address,
+    switchToNetwork,
+    connectToWallet
+  } = useWallet();
 
   return (
     <div className="App">
---
+++ src/WalletContext.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { ethers } from 'ethers';
+import { web3Config } from './config';
+
+const WalletContext = React.createContext({});
+
+export const WalletProvider = ({ children }) => {
+  const [provider, setProvider] = useState();
+  const [rpcProvider, setRpcProvider] = useState();
+  const [address, setWalletAddress] = useState('');
+
+  useEffect(() => {
+    const checkConnectedWallet = async () => {
+      const signer = provider.getSigner();
+      try {
+        const address = await signer.getAddress();
+        setWalletAddress(address);
+      } catch (e) {
+        console.error(e.message);
+      }
+    }
+
+    const listenToWalletAddressChange = async () => {
+      const ethProvider = await detectEthereumProvider();
+      ethProvider.on('accountsChanged', (accounts) => {
+        setWalletAddress(accounts[0]);
+      });
+    }
+
+    const listenToNetworkChange = async () => {
+      const ethProvider = await detectEthereumProvider();
+      ethProvider.on('chainChanged', (networkId) => {
+
+      });
+    }
+
+    if (provider) {
+      checkConnectedWallet();
+      listenToWalletAddressChange();
+      listenToNetworkChange();
+    }
+  }, [provider]);
+
+  useEffect(() => {
+    const initProvider = async () => {
+      const ethProvider = await detectEthereumProvider();
+      if (!ethProvider) {
+        console.error('Metamask is not installed');
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(ethProvider);
+      setProvider(provider);
+    };
+
+    const initRpcProvider = () => {
+      const provider = new ethers.providers.JsonRpcProvider(web3Config.rpcUrl);
+      setRpcProvider(provider);
+    }
+
+    initProvider();
+    initRpcProvider();
+  }, []);
+
+  const connectToWallet = async () => {
+    try {
+      await provider.send('eth_requestAccounts', []);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setWalletAddress(address);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  const switchToNetwork = async () => {
+    const { chainId } = web3Config;
+  
+    try {
+      await provider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: `0x${chainId}`}],
+      })
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  const contextValue = {
+    address,
+    provider,
+    rpcProvider,
+    switchToNetwork,
+    connectToWallet
+  };
+  console.log(contextValue);
+
+  return (
+    <WalletContext.Provider value={contextValue}>
+      {children}
+    </WalletContext.Provider>
+  )
+};
+
+export const useWallet = () => {
+  const context = React.useContext(WalletContext);
+  if (!context) {
+    throw new Error("This hook must be used inside a WalletContextProvider");
+  }
+  return context;
+};
