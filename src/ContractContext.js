c295c54052197f8feefbd894c2bc0ba0e0271470
Kevin Dave <kevindavee1505@gmail.com>
2022-04-25 23:15:50 +0700

    use useWallet

--- src/ContractContext.js
+++ src/ContractContext.js
@@ -2,10 +2,12 @@ import { ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
 import { web3Config } from './config';
 import Abi from './abi.json';
+import { useWallet } from './WalletContext';
 
 const ContractContext = React.createContext({});
 
-export const ContractContextProvider = ({ children, provider, rpcProvider }) => {
+export const ContractContextProvider = ({ children }) => {
+  const { provider, rpcProvider } = useWallet();
   const [contract, setContract] = useState(null);
   const [readContract, setReadContract] = useState(null);
 
