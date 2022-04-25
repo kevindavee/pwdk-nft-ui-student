669669c2a66b214491a2d95125c3ace429e48f0d
Kevin Dave <kevindavee1505@gmail.com>
2022-04-25 19:29:27 +0700

    init provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  const [provider, setProvider] = useState();
+
+  useEffect(() => {
+    const initProvider = async () => {
+      const ethProvider = await detectEthereumProvider();
+      if (!ethProvider) {
+        console.error('Metamask is not installed');
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(ethProvider);
+      setProvider(provider);
+    }
+
+    initProvider();
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
