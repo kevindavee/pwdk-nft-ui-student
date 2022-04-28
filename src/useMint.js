c139f229cc774ab1aa6470606d8ee5b5ad26d209
Kevin Dave <kevindavee1505@gmail.com>
2022-04-28 20:02:41 +0700

    implement useMint

---
+++ src/useMint.js
@@ -0,0 +1,37 @@
+import { ethers } from 'ethers';
+import { useCallback, useState } from 'react';
+import { useContract } from './ContractContext';
+import { useWallet } from './WalletContext';
+
+export const useMint = () => {
+  const { contract } = useContract();
+  const { address } = useWallet();
+  const getContract = useCallback(contract, [contract]);
+  
+  const [loading, setLoading] = useState(false);
+
+  const mint = async () => {
+    try {
+      const signContract = getContract('signing');
+      setLoading(true);
+      const tx = await signContract.mint({
+        value: ethers.utils.parseEther("0.01")
+      });
+      const receipt = await tx.wait();
+      const tokens = await signContract.tokensOfOwner(address);
+      return {
+        tokenId: tokens[tokens.length - 1],
+        transactionHash: receipt.transactionHash
+      };
+    } catch (e) {
+      setLoading(false);
+      console.error(e.message);
+      throw e;
+    }
+  };
+
+  return {
+    mint,
+    loading
+  };
+};
