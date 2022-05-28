b22e533d668b143d59565be2de5c4d4a8621183c
Kevin Dave <kevindavee1505@gmail.com>
2022-05-28 11:45:08 +0700

    mint individual ticket

--- src/ConferenceContext.js
+++ src/ConferenceContext.js
@@ -7,9 +7,10 @@ import Abi from './abis/conference.json';
 const ConferenceContext = React.createContext({});
 
 export const ConferenceContextProvider = ({ children }) => {
-  const { provider, rpcProvider } = useWallet();
+  const { provider, rpcProvider, address } = useWallet();
   const [contract, setContract] = useState(null);
   const [readContract, setReadContract] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (rpcProvider) {
@@ -29,8 +30,30 @@ export const ConferenceContextProvider = ({ children }) => {
     }
   }, [provider]);
 
+  const mintIndividualTicket = async () => {
+    try {
+      setLoading(true);
+      const tx = await contract.mintTicket();
+      const receipt = await tx.wait();
+
+      const tokens = readContract.tokensOfOwner(address);
+      setLoading(false);
+      return {
+        tokenIds: tokens
+          .slice(Math.max(tokens.length - 1, 0))
+          .map((t) => t.toNumber()),
+        transactionHash: receipt.transactionHash
+      };
+    } catch (e) {
+      setLoading(false);
+      console.error(e.message);
+      return;
+    }
+  }
+
   const contextValue = {
     contract: (purpose) => purpose === 'signing' ? contract : readContract,
+    mintIndividualTicket,
   };
 
   return (
